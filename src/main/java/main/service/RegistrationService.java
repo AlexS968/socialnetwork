503f26609bd9da25b3c412f416f2ad0de5e15347
vadim <vadim.frants42@gmail.com>
2020-09-22 19:02:37 +0300

    add registration

---
+++ src/main/java/main/service/RegistrationService.java
@@ -0,0 +1,59 @@
+package main.service;
+
+
+import lombok.AllArgsConstructor;
+import main.data.request.RegistrationRequest;
+import main.data.response.RegistrationResponse;
+import main.data.response.type.DataMessage;
+import main.exception.BadRequestException;
+import main.exception.apierror.ApiError;
+import main.model.MessagesPermission;
+import main.model.Person;
+import main.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+    private final PersonRepository personRepository;
+    public RegistrationResponse registrationNewPerson(RegistrationRequest request){
+        if (personRepository.findByEmail(request.getEmail()) != null) {
+            throw new BadRequestException(new ApiError(
+                    "invalid_request",
+                    "такой email уже существует"
+            ));
+        }
+        if (!(request.getPasswd1().equals(request.getPasswd2()))){
+            throw new BadRequestException(new ApiError(
+                    "invalid_request",
+                    "пароли не совпадают"
+            ));
+        }
+        if (!(request.getCode().equals("3675"))){
+            throw new BadRequestException(new ApiError(
+                    "invalid_request",
+                    "код не совпадает"
+            ));
+        }
+        Person person = new Person();
+        person.setEmail(request.getEmail());
+        person.setPasswordHash(String.valueOf(request.getPasswd1().hashCode()));
+        person.setFirstName(request.getFirstName());
+        person.setLastName(request.getLastName());
+        person.setRegDate(Instant.now());
+        person.setMessagesPermission(MessagesPermission.ALL);
+        person.setPhone("+71238492");
+        personRepository.save(person);
+        return new RegistrationResponse(
+                "",
+                LocalDateTime.now().toEpochSecond(ZoneOffset.UTC),
+                new DataMessage("ok")
+        );
+    }
+
+}
