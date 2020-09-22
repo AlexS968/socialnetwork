eaad55cded8f8f8dddd786d68cec24d56036e1e3
AlexS968 <61356947+AlexS968@users.noreply.github.com>
2020-09-22 11:20:34 +0200

    delete migration

--- src/main/resources/db/migration/V3__ins_test_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO person (e_mail, first_name, is_approved, is_blocked, last_name, messages_permission, password, reg_date)
-VALUES ('a.sablin@mail.ru', 'Alex', 1, 0,'Sablin', 'ALL', '111111', CURRENT_DATE);
