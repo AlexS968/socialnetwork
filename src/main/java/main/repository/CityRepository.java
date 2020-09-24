4f53649171599c2bd3347ccd645efd28f27ec3a6
v7777777 <vittaminka89@mail.ru>
2020-09-24 13:58:40 +0300

    open edit without photo and delete profile

--- src/main/java/main/repository/CityRepository.java
+++ src/main/java/main/repository/CityRepository.java
@@ -3,6 +3,7 @@ package main.repository;
 import main.model.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface CityRepository extends PagingAndSortingRepository<City, Integer> {
     Page<City> findByCountryId(int countryId, Pageable pageable);
     Page<City> findByCountryIdAndTitleIgnoreCaseContaining(int countryId, String title, Pageable pageable);
+    @Query(nativeQuery = true, value = "select * from city where city.title = :cityTitle and country_id = :countryId")
+    City findByCityTitleAndCountryId(String cityTitle, int countryId);
 }
--- src/main/java/main/repository/CountryRepository.java
+++ src/main/java/main/repository/CountryRepository.java
@@ -3,6 +3,7 @@ package main.repository;
 import main.model.Country;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface CountryRepository extends PagingAndSortingRepository<Country, Integer> {
     Page<Country> findAll(Pageable pageable);
     Page<Country> findByTitleIgnoreCaseContaining(String title, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "select * from country where country.title = :countryTitle")
+    Country findByCountryTitle(String countryTitle);
 }
