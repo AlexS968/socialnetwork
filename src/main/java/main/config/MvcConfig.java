4ff54713e82d43eb36588a49d3125af270adf1e9
Sergey Esipov <esipov.sergey.v@yandex.ru>
2020-10-23 18:57:31 +0300

    fix dialogs

--- src/main/java/main/config/MvcConfig.java
+++ src/main/java/main/config/MvcConfig.java
@@ -9,21 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-  @Value("${upload.path}")
-  String uploadPath;
+    @Value("${upload.path}")
+    String uploadPath;
 
-  @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/img/**").
-        addResourceLocations("file:///" + uploadPath + "/");
-  }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**").
+                addResourceLocations("file:///" + uploadPath + "/");
+    }
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "DELETE", "PUT")
-            .allowCredentials(true);
-  }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowCredentials(true);
+    }
 
 }
