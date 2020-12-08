1b660b6b77f5fec7d2331a7530487504b062d2e9
S0rago <linehalt10909@yandex.ru>
2020-12-08 22:54:35 +0300

    Swagger fortags and likes

--- src/main/java/main/controller/LikesController.java
+++ src/main/java/main/controller/LikesController.java
@@ -1,11 +1,15 @@
 package main.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import main.config.SpringFoxConfig;
 import main.data.request.LikeRequest;
 import main.data.response.base.Response;
 import main.data.response.type.LikesWithUsers;
+import main.exception.apierror.ApiError;
 import main.service.LikesService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,11 @@ import org.springframework.web.bind.annotation.*;
 public class LikesController {
     private final LikesService likesService;
 
+    @ApiOperation(value = "Ставил ли лайк пользователь на пост/комментарий")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = ""),
+            @ApiResponse(code = 401, message = "unauthorized", response = ApiError.class)
+    })
     @GetMapping("/liked")
     public ResponseEntity<Response<LikesWithUsers>> isLiked(
             @RequestParam(name = "user_id", required = false) Integer userId,
@@ -26,6 +35,11 @@ public class LikesController {
         return ResponseEntity.ok(likesService.isLiked(userId, itemId, type));
     }
 
+    @ApiOperation(value = "Получить лайки к посту")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = ""),
+            @ApiResponse(code = 401, message = "unauthorized", response = ApiError.class)
+    })
     @GetMapping("/likes")
     public ResponseEntity<Response<LikesWithUsers>> getLikes(
             @RequestParam(name = "item_id") int itemId,
@@ -34,6 +48,11 @@ public class LikesController {
         return ResponseEntity.ok(likesService.getLikes(itemId, type));
     }
 
+    @ApiOperation(value = "Поставить лайк")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = ""),
+            @ApiResponse(code = 401, message = "unauthorized", response = ApiError.class)
+    })
     @PutMapping("/likes")
     public ResponseEntity<Response<LikesWithUsers>> setLike(
             @RequestBody LikeRequest request
@@ -41,6 +60,11 @@ public class LikesController {
         return ResponseEntity.ok(likesService.setLike(request));
     }
 
+    @ApiOperation(value = "Удалить лайк")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = ""),
+            @ApiResponse(code = 401, message = "unauthorized", response = ApiError.class)
+    })
     @DeleteMapping("/likes")
     public ResponseEntity<Response<LikesWithUsers>> deleteLike(
             @RequestParam(name = "item_id") int itemId,
--- src/main/java/main/controller/TagController.java
+++ src/main/java/main/controller/TagController.java
@@ -1,6 +1,7 @@
 package main.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import main.config.SpringFoxConfig;
 import main.data.request.TagRequest;
@@ -19,6 +20,7 @@ public class TagController {
 
     private final TagService tagService;
 
+    @ApiOperation(value = "Получение тэгов к посту")
     @GetMapping("/")
     public ResponseEntity<ListResponse<SingleTag>> getPostTags(@RequestParam String tag,
                                                                @RequestParam int offset,
@@ -26,13 +28,15 @@ public class TagController {
         return ResponseEntity.ok(tagService.getPostTags(tag, offset, itemsPerPage));
     }
 
+    @ApiOperation(value = "Создфть новый тэг")
     @PostMapping("/")
-    public ResponseEntity<Response<SingleTag>> CreateTag(@RequestBody TagRequest request) {
+    public ResponseEntity<Response<SingleTag>> createTag(@RequestBody TagRequest request) {
         return ResponseEntity.ok(tagService.createTag(request));
     }
 
+    @ApiOperation(value = "Удалить тэг")
     @DeleteMapping("/")
-    public ResponseEntity<Response<DataMessage>> DeleteTag(@RequestParam int id) {
+    public ResponseEntity<Response<DataMessage>> deleteTag(@RequestParam int id) {
         return ResponseEntity.ok(tagService.deleteTag(id));
     }
 }
